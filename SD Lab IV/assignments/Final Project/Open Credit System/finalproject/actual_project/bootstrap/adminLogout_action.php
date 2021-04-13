ba7de4f46915c5fca80199cbf2f15bdb7f344fd2
Ashna-Nawar-Ahmed <82396321+Ashna-Nawar-Ahmed@users.noreply.github.com>
2021-04-13 22:06:00 +0600

    Delete SD Lab IV/assignments/Final Project/Open Credit System directory

--- SD Lab IV/assignments/Final Project/Open Credit System/finalproject/actual_project/bootstrap/adminLogout_action.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-echo '<script type="text/javascript">
-		window.confirm("You have successfully logged out");
-		location="loginAdmin.php";
-		</script>';
-?>
